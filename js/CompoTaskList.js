53c9af32af2acf8e20aacfb37d8486b14e48a6eb
Ploupipok <Wxfolios@gmail.com>
2019-03-14 10:27:00 +0100

    modif2_JSDoc

--- js/CompoTaskList.js
+++ js/CompoTaskList.js
@@ -1,16 +1,44 @@
+// écriture en ES6 avec le mot-clé class
+/* Création de la class "CompoTaskList" */
+/* Utilisation de la méthode "constructor" pour initialiser un objet */
+/* Affecte "param" à "this" */
+
 class CompoTaskList {
     constructor(param) {
         this.param = param;
     }
 
+    /**
+     * Retourne la tâche passée en paramètre au constructeur
+     */
     getTaskList() {
         return this.param.taskList;
     }
 
+    /**
+     * Retourne l'élément du DOM passé en paramètre au constructeur
+     */
     getElement() {
         return this.param.element;
     }
 
+    /**
+     * @class
+     * @summary Composant qui affiche une tâche
+     * Les paramètres obligatoire, à transmettre sous la forme
+     * d'un objet, sont :
+     * - task : objet Task à afficher
+     * - element : objet HTMLElement du DOM dans lequel
+     *   le code HTML doit être affiché
+     * @param {*} param - Objet de paramétrage
+     * @example
+     * let tk = new Task('test', null);
+     * let compoTk = new CompoTask({
+     *    task: tk,
+     *    element: document.getElementById('task')
+     * }) 
+     * compoTk.render();
+     */
     render() {
         let children = [];
         if (this.param.taskList) {
