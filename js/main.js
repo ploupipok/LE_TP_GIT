88766cd077b91efcf3f09bafc916eaf026e3be58
Ploupipok <Wxfolios@gmail.com>
2019-03-14 11:03:25 +0100

    Modif3_utilisation_objet

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ let tasks = new CompoTaskList({
     element: document.getElementById('tasks'),
     taskList: list
 });
+
+let task1 = new Task('task1', null);
+let task2 = new Task('task2', null);
+let task3 = new Task('task3', null);
+
+list.add(task1);
+list.add(task2);
+list.add(task3);
+
+tasks.render();
+
